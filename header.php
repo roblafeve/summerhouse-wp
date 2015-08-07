5de7ac4f78dfc4d836eb4705df95c1464a3b0e7e
Rob LaFeve <rob@cremalab.com>
2015-08-06 23:53:46 -0500

    Add facebook open graph tags

--- header.php
+++ header.php
@@ -3,11 +3,13 @@
   <head>
     <?php wp_head(); ?>
     <meta charset="UTF-8"/>
-    <title>Summer House</title>
+    <title>Summer House <?php echo the_title(); ?></title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width"/>
     <link rel="stylesheet" href="styles/style.css"/>
   </head>
   <body>
+
+    <?php get_template_part('facebook') ?>
 
     <?php get_template_part('titlebar') ?>
 
